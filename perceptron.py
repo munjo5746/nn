73cc197bdd09d1e72410468a80977c09db2f7a0a
Edward Chung <munjo5746@gmail.com>
2020-01-15 23:38:37 -0500

    learn(...) function has been added. This function takes a 2-d matrix data and learns (outputs w)

--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,8 @@
 import numpy as np
-
+import random
 # matrix is 2D array
 # w is 3-D numpy array
+# this function returns True or False which tells if the algorithm converged or not
 
 
 def convergence(matrix, w):
@@ -21,3 +22,34 @@ def convergence(matrix, w):
             return False
 
     return True
+
+
+# This function takes a 2-d matrix data and returns the weight vector.
+def learn(matrix):
+    iterationLimit = 20
+    w = np.array([1, 1, 1])
+
+    while iterationLimit > 0 and not convergence(matrix, w):
+        row = matrix[random.randrange(0, len(matrix))]
+
+        if len(row) != 3:
+            raise Exception('A row in the matrix is invalid.')
+
+        point = row[:2]
+        label = row[2]
+
+        dotProduct = np.dot(w, np.insert(point, 1, 1))
+
+        # update the weights
+        if label == 1 and dotProduct < 0:
+            w = np.add(w, np.insert(point, 1, 1))
+
+        if label == 0 and dotProduct >= 0:
+            w = np.subtract(w, np.insert(point, 1, 1))
+
+        iterationLimit -= 1
+
+    if iterationLimit == 0:
+        raise Exception('Possibly, not learned the data.')
+
+    return w
